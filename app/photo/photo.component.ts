bbb531bbcd910aed31f69f032da558096e683a3e
NathPaiva <hello@nathpaiva.com.br>
2017-06-17 09:21:11 -0300

    create a photo component

---
+++ app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'photo',
+  templateUrl: './photo.component.html'
+})
+export class PhotoComponent {
+  @Input() url;
+  @Input() titulo;
+}
